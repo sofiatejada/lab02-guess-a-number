39565aa56a332ea39a273513f805666f8a7b97d5
Sofia Tejada <sofianais@icloud.com>
2021-05-05 09:14:20 -0700

    get the scoring to work

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const wrongDisplay = document.getElementById('tries-remaining');
 const result = document.getElementById('result');
 
 const correctNumber = Math.ceil(Math.random() * 20);
-
+let score = 0;
 
 //set up an event listener
 
 button.addEventListener('click', () => {
     //track the user inputs
-    let score = 0;
+    score = score + 1;
 
     
     //get the user input and store it in a variable
@@ -33,16 +33,16 @@ button.addEventListener('click', () => {
     //check the input against the random number
 
     if (compareNumbers(input, correctNumber) === 0) {
-        wrongDisplay.textContent = 'Well done! You\'re right!';
+        result.textContent = `Well done! You're right! You have clicked ${score} times`;
     } else if (compareNumbers(input, correctNumber) === 1) {
-        score = score + 1;
-        wrongDisplay.textContent = 'Too high! Try again.';
+        
+        wrongDisplay.textContent = `Too high! Try again. You have clicked ${score} times`;
     } else if (compareNumbers(input, correctNumber) === -1) {
         
-        wrongDisplay.textContent = 'Too low! Try again.';
+        wrongDisplay.textContent = `Too low! Try again. You have clicked ${score} times`;
     }
 
-    console.log(score);
+    
     
 
     
