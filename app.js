22cd01d00c011fe77f0279f869ce26f227f1b076
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-07 13:06:04 -0700

    refactored and added matches played counter

--- app.js
+++ app.js
@@ -14,19 +14,25 @@ const section = document.getElementById('section');
 const wins = document.getElementById('times-won');
 const loses = document.getElementById('times-lost');
 const playAgainButton = document.getElementById('play-again-button');
+const timesPlayedDisplay = document.getElementById('times-played');
+
 
 let correctNumber = Math.ceil(Math.random() * 20);
 let score = 0;
 let timesWon = 0;
-let timesLost = 0;
-
+let timesPlayed = 0;
+let timesLost = 0; 
 //set up an event listener
 
 button.addEventListener('click', () => {
     //track the user inputs every time there's a click
-    score = score + 1;
-
+    score = score + 1; 
     
+    timesPlayed = score;
+    timesPlayedDisplay.textContent = timesPlayed;
+    
+    // console.log(score, timesPlayed);
+    // console.log(typeof score);
     //get the user input and store it in a variable
     const input = Number(userInput.value);
     //console.log(userInput.value);
@@ -46,36 +52,52 @@ button.addEventListener('click', () => {
             result.textContent = `Well done! You won in ${score} try!`;
             button.disabled = true;
         }
+
         //unhide it
         section.style.display = 'block';
         result.style.display = 'block';
+
         //set result back to zero
         score = 0;
+
         //generate a new number
         correctNumber = Math.ceil(Math.random() * 20);
+
         //tracking wins
         timesWon = timesWon + 1;
+        
         //display times won
         wins.textContent = timesWon;
+        
         //hide wrong display
         wrongDisplay.style.display = 'none';
+    
     } else if (compareNumbers(input, correctNumber) === 1) {
         wrongDisplay.textContent = `Too high! Try again. You have ${5 - score} tries remaining`;
+        
         //unhide it
         section.style.display = 'block';
         wrongDisplay.style.display = 'block';
+        
         //display times won
         wins.textContent = timesWon;
+
         //hide winning message
+
         result.style.display = 'none';
     } else if (compareNumbers(input, correctNumber) === -1) {     
         wrongDisplay.textContent = `Too low! Try again. You have ${5 - score} tries remaining`;
         //display times won
+
         wins.textContent = timesWon;
         //unhide it
+
         section.style.display = 'block';
+
+
         wrongDisplay.style.display = 'block';
         //hide winning message
+
         result.style.display = 'none';
     }
 
@@ -83,15 +105,23 @@ button.addEventListener('click', () => {
 
     if (score === 5) {
         //tracking loses
-        timesLost = timesLost + 1;
+        
+
+        timesLost++;
         //display times lost
+
+
         loses.textContent = timesLost;
+
         wrongDisplay.textContent = 'You lose! Try again.';
         //reset score to zero
+
         score = 0;
         //generate a new number
+
         correctNumber = Math.ceil(Math.random() * 20);
         //turn off button
+
         button.disabled = true;
     }
     
@@ -105,32 +135,48 @@ button.addEventListener('click', () => {
 
 reset.addEventListener('click', () => {
     //reset score to zero
+
+    timesLost = 0;
+
     score = 0;
     //reset times won
+
     timesWon = 0;
     wins.textContent = timesWon;
     //reset times lost
-    timesLost = 0;
+
+    timesLost;
     loses.textContent = timesLost;
     //generate a new number
+
     correctNumber = Math.ceil(Math.random() * 20);
     //hide displays again
+
     section.style.display = 'none';
     //console.log(correctNumber);
+
     //button works again
+
     button.disabled = false;
+
 });
 
 //play again button
 
 playAgainButton.addEventListener('click', () => {
     //reset score to zero
+    
     score = 0;
     //generate a new number
+
     correctNumber = Math.ceil(Math.random() * 20);
     //hide displays again
+
     section.style.display = 'none';
     //console.log(correctNumber);
+
     //button works again
+
     button.disabled = false;
+
 });
